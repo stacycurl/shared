050c2b56ecaff3e745040a3d4a0133fe4036fc0c
Stacy Curl <stacy.curl@gmail.com>
2014-03-09 00:52:16 +0000

    Combined Reader type classes into single object, added Comonad, Cozip, Monad & Traverse

--- core/src/main/scala/stacycurl/scala/shared/shared.scala
+++ core/src/main/scala/stacycurl/scala/shared/shared.scala
@@ -197,12 +197,24 @@ class ZippedLock(left: Lock, right: Lock) extends Lock {
 object Reader {
   implicit def readerAsA[A](ra: Reader[A]): A = ra.get()
 
-  implicit object ReaderFunctor extends Functor[Reader] {
-    def map[A, B](ra: Reader[A])(f: A => B): Reader[B] = ra.map(f)
-  }
+  implicit object ReaderInstance extends Comonad[Reader] with Cozip[Reader] with Monad[Reader]
+    with Traverse[Reader] with Zip[Reader] {
 
-  implicit object readerZip extends Zip[Reader] {
+    def point[A](a: => A): Reader[A] = FunctionReader[A](() => a)
+    def copoint[A](ra: Reader[A]): A = ra.get()
+
+    def bind[A, B](ra: Reader[A])(f: A => Reader[B]): Reader[B] = point(f(ra.get()).get())
+    def cobind[A, B](ra: Reader[A])(f: Reader[A] => B): Reader[B] = point(f(ra))
+
+    def traverseImpl[G[_]: Applicative, A, B](ra: Reader[A])(f: A => G[B]): G[Reader[B]] =
+      Functor[G].map[B, Reader[B]](f(ra.get()))((b: B) => point(b))
+
     def zip[A, B](ra: => Reader[A], rb: => Reader[B]): Reader[(A, B)] = ra.zip(rb)
+
+    def cozip[A, B](rab: Reader[A \/ B]): Reader[A] \/ Reader[B] =
+      rab.get().bimap((a: A) => point(a), (b: B) => point(b))
+
+    override def map[A, B](ra: Reader[A])(f: A => B): Reader[B] = ra.map(f)
   }
 }
 
@@ -218,6 +230,10 @@ case class MappedReader[A, +B](ra: Reader[A], f: A => B) extends Reader[B] {
 
 case class ZippedReader[+A, +B](ra: Reader[A], rb: Reader[B]) extends Reader[(A, B)] {
   def get(): (A, B) = (ra.get(), rb.get())
+}
+
+case class FunctionReader[A](f: () => A) extends Reader[A] {
+  def get(): A = f()
 }
 
 object Change {
