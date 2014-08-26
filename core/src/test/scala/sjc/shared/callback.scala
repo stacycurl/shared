df4375a1c3855175ab12123ff00228bcce21154e
Stacy Curl <stacy.curl@gmail.com>
2014-08-26 08:41:59 +0100

    Added check of lawfulness of Contravariant[Callback], + increased coverage

--- core/src/test/scala/sjc/shared/callback.scala
+++ core/src/test/scala/sjc/shared/callback.scala
@@ -1,9 +1,10 @@
 package sjc.shared
 
 import org.junit.Test
+import org.scalacheck._
+import scalaz._
 
 import org.junit.Assert._
-import scalaz._
 
 
 class CallbackTests {
@@ -39,4 +40,27 @@ class CallbackTests {
     filtered(Change(1, 3))
     assertEquals(List(Change(1, 2), Change(1, 3)), intChanges.get())
   }
+
+  @Test def canContramap {
+    val ints = Shared[List[Change[Int]]](Nil)
+    val intCallback = Callback[Int](ints += _)
+    val stringCallback = Callback.CallbackContravariant.contramap[Int, String](intCallback)(_.toInt)
+
+    stringCallback(Change("123", "456"))
+    assertEquals(List(Change(123, 456)), ints.get())
+  }
+}
+
+object CallbackSpec extends BaseSpec("Callback") {
+  import scalaz.scalacheck.ScalazProperties._
+  import ChangeSpec._
+
+  implicit val intCallback: Arbitrary[Callback[Int]] = Arbitrary {
+    Gen.const(Callback[Int](_ => ()))
+  }
+
+  implicit def equalIntCallback: Equal[Callback[Int]] =
+    Equal.equalBy[Callback[Int], Change[Int]](_.apply(Change(123, 456)))
+
+  checkAll(contravariant.laws[Callback])
 }
