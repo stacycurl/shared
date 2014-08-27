0617ed064df755775a5ad211401e323a31dcbe72
Stacy Curl <stacy.curl@gmail.com>
2014-08-27 19:24:33 +0100

    Removed SharedLock

--- core/src/main/scala/sjc/shared/lock.scala
+++ core/src/main/scala/sjc/shared/lock.scala
@@ -10,8 +10,7 @@ trait Lock {
   def zip(other: Lock): Lock = ZippedLock(this, other)
   def tap(callback: LockEvent => Unit): Lock = TappedLock(this, callback)
 
-  private[shared] val isStable: Boolean
-  private[shared] def identity: Int
+  private[shared] val identity: Int
   private[shared] def locks: Stream[Lock]
 }
 
@@ -19,7 +18,6 @@ case class Synchronized(monitor: AnyRef = new Object) extends Lock {
   def withRead[A](f: => A): A  = monitor.synchronized(f)
   def withWrite[A](f: => A): A = monitor.synchronized(f)
 
-  private[shared] val isStable = true
   private[shared] val identity = System.identityHashCode(monitor)
   private[shared] val locks = Stream(this)
 }
@@ -31,7 +29,6 @@ case class ReadWriteLock(lock: JRWLock = new JRRWLock()) extends Lock {
   private def withLock[A](lock: JLock, f: => A): A =
     try { lock.lock(); f } finally { lock.unlock() }
 
-  private[shared] val isStable = true
   private[shared] val identity = System.identityHashCode(lock)
   private[shared] val locks = Stream(this)
 }
@@ -41,7 +38,6 @@ case object Unlocked extends Lock {
   def withWrite[A](f: => A): A = f
   override def zip(other: Lock): Lock = other
 
-  private[shared] val isStable = true
   private[shared] val identity = System.identityHashCode(this)
   private[shared] val locks = Stream(this)
 }
@@ -56,14 +52,11 @@ case class ZippedLock(left: Lock, right: Lock) extends Lock {
       case _             => f
     }
 
-    recurse(if (isStable) stableLocks else locks)
+    recurse(locks)
   }
 
-  private[shared] val isStable = left.isStable && right.isStable
   private[shared] val identity = System.identityHashCode(this)
   private[shared] def locks = (left.locks ++ right.locks).sortBy(_.identity)
-
-  private lazy val stableLocks = locks
 }
 
 case class TappedLock(lock: Lock, callback: LockEvent => Unit) extends Lock {
@@ -77,27 +70,8 @@ case class TappedLock(lock: Lock, callback: LockEvent => Unit) extends Lock {
     try lock.withWrite(f) finally callback(LeavingWrite(lock))
   }
 
-  private[shared] val isStable = lock.isStable
-  private[shared] def identity = lock.identity
-  private[shared] def locks = if (isStable) stableLocks else tapLocks
-
-  private lazy val stableLocks = tapLocks
-  private def tapLocks = lock.locks.map(_.tap(callback))
-}
-
-// Not sure if this is useful at all, just 'closing' over the types defined
-class SharedLock(value: Shared[Lock] = Shared(Synchronized())) extends Lock with Shared[Lock] {
-  def withRead[A](f: => A): A = value.get().withRead[A](f)
-  def withWrite[A](f: => A): A = value.get().withWrite[A](f)
-
-  def get(): Lock = value.get()
-  def lock: Lock = value.lock
-  def alter(f: Lock => Change[Lock]): Change[Lock] = value.alter(f)
-  def onChange(callback: Callback[Lock]) = { value.onChange(callback); this }
-
-  private[shared] val isStable = false
-  private[shared] def identity = value.get().identity
-  private[shared] def locks = value.get().locks
+  private[shared] val identity = lock.identity
+  private[shared] def locks = lock.locks.map(_.tap(callback))
 }
 
 sealed trait LockEvent
